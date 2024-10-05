907c0943f505b1a7aa469a05f55a81ae8257bbef
dn28203 <ntkngann2k3@gmail.com>
2024-10-05 16:50:25 +0700

    change port to 8800

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,5 +4,5 @@ app = Flask("app")
 CORS(app)
 
 from app.controllers import *
-from app.packages.auth.controllers import auth_controller, face_controller
-from app.packages.users.controllers import user_controller
+from app.packages.auth.controllers import auth_controller
+from app.packages.staff.controllers import staff_controller
