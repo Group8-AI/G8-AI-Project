05ed91e7274015a08b266de682e1ef37ac96f93c
Group8-AI <group8.aiprojects@gmail.com>
2024-10-26 13:47:24 +0700

    file pth

--- backend/app/utils/signature_processor.py
+++ backend/app/utils/signature_processor.py
@@ -1,10 +1,47 @@
 import tensorflow as tf
+import torch
+import torch.nn as nn
 import numpy as np
+import torchvision.models as models
 from tensorflow.keras.preprocessing import image
+
+class SiameseResNet(nn.Module):
+    def __init__(self):
+        super(SiameseResNet, self).__init__()
+        # Load pretrained VGG16 model
+        self.vgg16 = models.vgg16(pretrained=True)
+
+        # Use only the feature extraction part of VGG16
+        self.feature_extractor = self.vgg16.features
+
+    def forward(self, input1, input2):
+        # Extract features from both inputs
+        output1 = self.feature_extractor(input1)
+        output2 = self.feature_extractor(input2)
+
+        # Flatten the output for both branches (VGG16 outputs a 3D tensor [batch_size, 512, 7, 7])
+        output1 = torch.flatten(output1, start_dim=1)
+        output2 = torch.flatten(output2, start_dim=1)
+
+        return output1, output2
 class SignatureProcessor:
     def __init__(self):
+        model_path = "app/model/model_vgg16.pth"
         # Load các model
-        self.verification_model = tf.keras.models.load_model('app/model/vgg16_finetuned_model.h5')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Khởi tạo mô hình từ lớp SiameseResNet
+        self.verification_model = SiameseResNet()
+
+        # Chuyển mô hình sang thiết bị
+        self.verification_model = self.verification_model.to(device)
+
+        # Tải trọng số mô hình
+        self.verification_model.load_state_dict(torch.load(model_path, map_location=device))
+
+        # Đặt mô hình về chế độ đánh giá
+        self.verification_model.eval()
+
         
 
     def preprocess_image(self, img_path):
@@ -16,8 +53,7 @@ class SignatureProcessor:
 
     def verify_signature(self, img1_array, img2_array):
         # So sánh 2 chữ ký
-        features1 = self.verification_model.predict(img1_array)
-        features2 = self.verification_model.predict(img2_array)
+        features1, features2 = self.verification_model(img1_array.to(device), img2_array.to(device))
         
         # Tính cosine similarity
         similarity = np.dot(features1.flatten(), features2.flatten()) / (
