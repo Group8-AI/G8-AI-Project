0425a61531e713d434fdc8da7072123a321bd527
dn28203 <ntkngann2k3@gmail.com>
2024-10-28 20:36:21 +0700

    update model

--- backend/app/utils/signature_processor.py
+++ backend/app/utils/signature_processor.py
@@ -4,21 +4,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import torchvision.models as models
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 from torchvision import transforms
 
 class SiameseResNet(nn.Module):
     def __init__(self):
         super(SiameseResNet, self).__init__()
-        # Load pretrained VGG16 model
-        self.vgg16 = models.vgg16(pretrained=True)
-        # Use only the feature extraction part of VGG16
-        self.feature_extractor = self.vgg16.features
+        # Load pretrained ResNet18 model
+        self.resnet = models.resnet18(pretrained=True)
+
+        # Remove the fully connected layer (we are only interested in feature extraction)
+        self.resnet = nn.Sequential(*(list(self.resnet.children())[:-1]))
 
     def forward(self, input1, input2):
         # Extract features from both inputs
-        output1 = self.feature_extractor(input1)
-        output2 = self.feature_extractor(input2)
+        output1 = self.resnet(input1)
+        output2 = self.resnet(input2)
+
 
         # Flatten the output for both branches
         output1 = torch.flatten(output1, start_dim=1)
@@ -28,7 +30,7 @@ class SiameseResNet(nn.Module):
 
 class SignatureProcessor:
     def __init__(self):
-        model_path = "app/model/model_vgg16.pth"
+        model_path = "app/model/model_resnet18_1.pth"
         # Set up the device
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -41,25 +43,29 @@ class SignatureProcessor:
         self.preprocess = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
     def preprocess_image(self, img_path):
         # Load and preprocess the image
-        img = image.load_img(img_path, target_size=(224, 224))
-        img_array = image.img_to_array(img)
-        img_tensor = self.preprocess(image.array_to_img(img_array)).unsqueeze(0)
+        img = Image.open(img_path)
+        img_tensor = self.preprocess(img)
         return img_tensor.to(self.device)
 
     def verify_signature(self, img1_tensor, img2_tensor):
 
-
+        features1 = None
+        features2 = None
+        img1_tensor = img1_tensor.unsqueeze(0)  # Add batch dimension
+        img2_tensor = img2_tensor.unsqueeze(0)
         with torch.no_grad():
             features1, features2 = self.verification_model(img1_tensor, img2_tensor)
-        
         # Calculate cosine similarity
         similarity = F.cosine_similarity(features1, features2)
-        
+        print (similarity)
         # Set threshold for verification
         threshold = 0.85
-        return similarity.item() > threshold, similarity.item()
+        preds = (similarity.mean() > threshold)
+
+            # Store predictions and true labels
+        print (preds)
+        return preds, similarity.mean()
